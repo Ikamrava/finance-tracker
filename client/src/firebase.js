8a7755bf8c353863d1045ea2ed5b30af99adfe8f
ikamrava <kamrava.iman@outlook.com>
2023-08-04 10:43:29 +0100

    add config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,22 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
+import { apiKey, authDomain, projectId,storageBucket,messagingSenderId,appId } from './config'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId:process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId:messagingSenderId,
+  appId: appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+
+
 
