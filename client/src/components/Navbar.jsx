3f795333eda9503e151654fe9b3752bbe79e86b2
ikamrava <kamrava.iman@outlook.com>
2023-08-04 08:46:32 +0100

    remove avatar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, redirect } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
-import Avatar from 'react-avatar';
+
 
 
 function Navbar() {
@@ -20,7 +20,6 @@ function Navbar() {
     <div className=" flex items-center justify-between px-10 h-14 bg-gray-400 mb-5">
       <h1>Expense Tracker</h1>
       <div className=" flex flex-col items-center">
-      <Avatar size="30" facebook-id="invalidfacebookusername" round="20px" src={user?.photoURL}  />
       {user?.displayName ?  (<button onClick={handlesignOut}>Sign Out</button>):(<Link to="/login"> SignIn </Link>)}
       </div>
       
