e96dc7fb21cdc51968fcf086eac33fc5c8e6f25e
ikamrava <kamrava.iman@outlook.com>
2023-08-04 10:09:48 +0100

    firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId:process.env.MESSAGING-SENDER_ID,
+  messagingSenderId:process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
 };
 
