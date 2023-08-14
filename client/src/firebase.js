f7413ef39b63b9d49d8222c494ab7e816a339967
ikamrava <kamrava.iman@outlook.com>
2023-08-14 13:51:39 +0100

    change vite env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,6 +2,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+const apiKey = import.meta.env.VITE_API_KEY;
+const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_PROJECT_ID;
+
+
 // import { apiKey, authDomain, projectId,storageBucket,messagingSenderId,appId } from './config'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,9 +15,9 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
 
 };
 
